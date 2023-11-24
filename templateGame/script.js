42dffe00dd701a1be4efe8f23b23f1ad6705db92
Nicholasp3435 <nap44794@uga.edu>
2023-11-24 01:28:32 -0500

    feat: Update isMouseOver for UI elements; fix: debugging box draws around UI elements

--- templateGame/script.js
+++ templateGame/script.js
@@ -3,6 +3,9 @@ import { Camera } from "../lib/Camera.js";
 
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
+let frameCount = 0;
+
+let debuggingMode = true;
 
 let allSprites = [];
 
@@ -13,10 +16,12 @@ sprite.setHitbox(216, 72);
 const sprite2 = createSprite(100,200);
 sprite2.setHitbox(128, 64, 0, 64);
 
-const sprite3 = createSprite(100,200);
+const sprite3 = createSprite(0,0);
 sprite3.addAnimation("apple", "appleFly.png", 72, 72, () => {
     sprite3.setAnimation("apple");
 });
+sprite3.setHitbox(72, 72);
+sprite3.isUI = true;
 
 
 const camera = new Camera(0,0);
@@ -33,13 +38,23 @@ function drawUI() {
 
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    const canvasMousePosition = getRelativeMousePosition(ctx, false);
+    const cameraMousePosition = getRelativeMousePosition(ctx, true);
     camera.lockOn(sprite2, canvas);
     ctx.save();
     ctx.translate(-camera.x, -camera.y);
     drawScene();
-    debugHitboxes();
+    debugSceneHitboxes();
     ctx.restore();
     drawUI();
+    debugUIHitboxes();
+    if (sprite2.mouseIsOver(canvasMousePosition)) {
+        console.log("Mouse is over the sprite");
+    }
+    if (sprite3.mouseIsOver(cameraMousePosition)) {
+        console.log("Mouse is over the sprite");
+    }
+    frameCount++;
 }
 
 sprite2.handleMovement = function() {
@@ -59,6 +74,8 @@ sprite2.handleMovement = function() {
 
 
 // util functions
+
+// for keyDown
 const keysPressed = {};
 document.addEventListener('keydown', (e) => {
     keysPressed[e.key] = true;
@@ -66,10 +83,47 @@ document.addEventListener('keydown', (e) => {
 document.addEventListener('keyup', (e) => {
     keysPressed[e.key] = false;
 });
-
 function keydown(key) {
     return keysPressed[key] === true;
 }
+
+// for mouseDown
+const mousePressed = {};
+document.addEventListener('mousedown', (e) => {
+    mousePressed[e.button] = true;
+});
+document.addEventListener('mouseup', (e) => {
+    mousePressed[e.button] = false;
+});
+function mouseDown(button) {
+    return mousePressed[button] === true;
+}
+
+// for mousePosition (and mouseIsOver)
+let rawMousePosition = { x: 0, y: 0 };
+canvas.addEventListener('mousemove', (event) => {
+    const rect = canvas.getBoundingClientRect();
+    rawMousePosition.x = event.clientX - rect.left;
+    rawMousePosition.y = event.clientY - rect.top;
+});
+function getRelativeMousePosition(ctx, ui) {
+    const transform = ctx.getTransform();
+    const invertedTransform = transform.inverse(); 
+
+    if (!ui) {
+        return {
+            x: rawMousePosition.x * invertedTransform.a + rawMousePosition.y * invertedTransform.c + invertedTransform.e + camera.x,
+            y: rawMousePosition.x * invertedTransform.b + rawMousePosition.y * invertedTransform.d + invertedTransform.f + camera.y
+        };
+    } else {
+        return {
+            x: rawMousePosition.x,
+            y: rawMousePosition.y
+        };
+    }
+
+}
+
 
 function createSprite(x, y) {
     const newSprite = new Sprite(x, y);
@@ -77,10 +131,24 @@ function createSprite(x, y) {
     return newSprite;
 }
 
-function debugHitboxes() {
-    allSprites.forEach(sprite => {
-        sprite.drawHitbox(ctx);
-    });
+function debugSceneHitboxes() {
+    if (debuggingMode) {
+        allSprites.forEach(sprite => {
+            if (!sprite.isUI) {
+                sprite.drawHitbox(ctx);
+            }
+        });
+    }
+}
+
+function debugUIHitboxes() {
+    if (debuggingMode) {
+        allSprites.forEach(sprite => {
+            if (sprite.isUI) {
+                sprite.drawHitbox(ctx);
+            }
+        });
+    }
 }
 
 const interval = setInterval(draw, 10);
